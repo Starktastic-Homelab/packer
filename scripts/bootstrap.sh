92fa4386c3707eccbc3e270e1b587b9b09925b52
MrStarktastic <benfaingold@gmail.com>
2025-08-12 15:04:31 +0300

    Remove resolvconf

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -17,7 +17,7 @@ apt full-upgrade -y
 # Install core dependencies
 # ----------------------------
 echo 'Installing core packages...'
-apt install -y --install-recommends curl cloud-init resolvconf
+apt install -y --install-recommends curl cloud-init
 
 # ----------------------------
 # Clean and remove unnecessary packages
